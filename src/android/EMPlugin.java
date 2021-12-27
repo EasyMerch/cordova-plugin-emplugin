dffe773dfe8f128da13156aa505d9ec9ae85aeb4
Lordovik <lordovikotaku@gmial.com>
2021-12-27 12:59:30 +0300

    locationIsMock

--- src/android/EMPlugin.java
+++ src/android/EMPlugin.java
@@ -1,7 +1,5 @@
 package ru.pronetcom.easymerch2.emplugin;
 
-import java.util.Objects;
-import java.util.TimeZone;
 import java.util.List;
 
 import java.io.File;
@@ -17,20 +15,25 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.Manifest;
+import android.os.Build;
 import android.util.Log;
-import android.provider.Settings;
+
+import android.location.Location;
+import android.location.LocationManager;
 
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 
+import androidx.core.app.ActivityCompat;
+
 public class EMPlugin extends CordovaPlugin {
 	public static final String TAG = "EMPlugin";
 	private static final String GETPROP_EXECUTABLE_PATH = "/system/bin/getprop";
 
-	CallbackContext callbackContext;
-	JSONArray args;
+	public static LocationManager locationManager = null;
 
 	/**
 	 * Constructor.
@@ -61,6 +64,7 @@ public class EMPlugin extends CordovaPlugin {
 		switch(action){
 			case "getDeviceInfo": getDeviceInfo(callbackContext); return true;
 			case "getMockPermissionApps": getMockPermissionApps(callbackContext); return true;
+			case "locationIsMock": getLocationIsMock(callbackContext); return true;
 		}
 
 		return false;
@@ -80,52 +84,52 @@ public class EMPlugin extends CordovaPlugin {
 
 	public boolean isVirtual() {
 		return android.os.Build.FINGERPRINT.contains("generic")
-			|| android.os.Build.PRODUCT.contains("sdk")
-			//
-			|| android.os.Build.FINGERPRINT.startsWith("generic")
-			|| android.os.Build.FINGERPRINT.startsWith("unknown")
-			|| android.os.Build.MODEL.contains("google_sdk")
-			|| android.os.Build.MODEL.contains("Emulator")
-			|| android.os.Build.MODEL.contains("Android SDK built for x86")
-			//nox
-			|| this.onNox()
-			//bluestacks
-			// || "QC_Reference_Phone".equals(android.os.Build.BOARD) && !"Xiaomi".equalsIgnoreCase(android.os.Build.MANUFACTURER)
-			|| this.onBlueStacks()
-			|| android.os.Build.MANUFACTURER.contains("Genymotion")
-			|| "Build2".equals(android.os.Build.HOST) //MSI App Player
-			|| android.os.Build.BRAND.startsWith("generic") && android.os.Build.DEVICE.startsWith("generic")
-			|| "google_sdk".equals(android.os.Build.PRODUCT)
-			// another Android SDK emulator check
-			|| "1".equals(EMPlugin.getSystemProperty("ro.kernel.qemu"));
+				|| android.os.Build.PRODUCT.contains("sdk")
+				//
+				|| android.os.Build.FINGERPRINT.startsWith("generic")
+				|| android.os.Build.FINGERPRINT.startsWith("unknown")
+				|| android.os.Build.MODEL.contains("google_sdk")
+				|| android.os.Build.MODEL.contains("Emulator")
+				|| android.os.Build.MODEL.contains("Android SDK built for x86")
+				//nox
+				|| this.onNox()
+				//bluestacks
+				// || "QC_Reference_Phone".equals(android.os.Build.BOARD) && !"Xiaomi".equalsIgnoreCase(android.os.Build.MANUFACTURER)
+				|| this.onBlueStacks()
+				|| android.os.Build.MANUFACTURER.contains("Genymotion")
+				|| "Build2".equals(android.os.Build.HOST) //MSI App Player
+				|| android.os.Build.BRAND.startsWith("generic") && android.os.Build.DEVICE.startsWith("generic")
+				|| "google_sdk".equals(android.os.Build.PRODUCT)
+				// another Android SDK emulator check
+				|| "1".equals(EMPlugin.getSystemProperty("ro.kernel.qemu"));
 	}
 
 	public String getInfo() {
 		return String.format(
-			"FINGERPRINT:%s\n"+
-			"MODEL:%s\n"+
-			"MANUFACTURER:%s\n"+
-			"BRAND:%s\n"+
-			"DEVICE:%s\n"+
-			"BOARD:%s\n"+
-			"HOST:%s\n"+
-			"PRODUCT:%s\n"+
-			"HARDWARE:%s\n"+
-			"onBlueStacks:%s\n"+
-			"onNox:%s\n"+
-			"ro.kernel.qemu:%s",
-			android.os.Build.FINGERPRINT,
-			android.os.Build.MODEL,
-			android.os.Build.MANUFACTURER,
-			android.os.Build.BRAND,
-			android.os.Build.DEVICE,
-			android.os.Build.BOARD,
-			android.os.Build.HOST,
-			android.os.Build.PRODUCT,
-			android.os.Build.HARDWARE,
-			this.onBlueStacks(),
-			this.onNox(),
-			EMPlugin.getSystemProperty("ro.kernel.qemu")
+				"FINGERPRINT:%s\n"+
+						"MODEL:%s\n"+
+						"MANUFACTURER:%s\n"+
+						"BRAND:%s\n"+
+						"DEVICE:%s\n"+
+						"BOARD:%s\n"+
+						"HOST:%s\n"+
+						"PRODUCT:%s\n"+
+						"HARDWARE:%s\n"+
+						"onBlueStacks:%s\n"+
+						"onNox:%s\n"+
+						"ro.kernel.qemu:%s",
+				android.os.Build.FINGERPRINT,
+				android.os.Build.MODEL,
+				android.os.Build.MANUFACTURER,
+				android.os.Build.BRAND,
+				android.os.Build.DEVICE,
+				android.os.Build.BOARD,
+				android.os.Build.HOST,
+				android.os.Build.PRODUCT,
+				android.os.Build.HARDWARE,
+				this.onBlueStacks(),
+				this.onNox(),
+				EMPlugin.getSystemProperty("ro.kernel.qemu")
 		);
 	}
 
@@ -159,24 +163,24 @@ public class EMPlugin extends CordovaPlugin {
 
 	public Boolean onBlueStacks(){
 		File sharedFolder = new File(android.os.Environment
-			.getExternalStorageDirectory().toString()
-			+ File.separatorChar
-			+ "windows"
-			+ File.separatorChar
-			+ "BstSharedFolder");
+				.getExternalStorageDirectory().toString()
+				+ File.separatorChar
+				+ "windows"
+				+ File.separatorChar
+				+ "BstSharedFolder");
 
 		return sharedFolder.exists();
 	}
 
 	public Boolean onNox(){
 		File noxFolder = new File(File.separatorChar
-		+ "storage"
-		+ File.separatorChar
-		+ "emulated"
-		+ File.separatorChar
-		+ "0"
-		+ File.separatorChar
-		+ "BigNoxGameHD"
+				+ "storage"
+				+ File.separatorChar
+				+ "emulated"
+				+ File.separatorChar
+				+ "0"
+				+ File.separatorChar
+				+ "BigNoxGameHD"
 		);
 
 		return noxFolder.exists();
@@ -215,10 +219,10 @@ public class EMPlugin extends CordovaPlugin {
 						// Check for System App //
 						if(!((applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 1)) {
 							if (!(
-								requestedPermissions[i].equals("android.permission.ACCESS_MOCK_LOCATION") &&
-								!applicationInfo.packageName.equals(context.getPackageName())
+									requestedPermissions[i].equals("android.permission.ACCESS_MOCK_LOCATION") &&
+											!applicationInfo.packageName.equals(context.getPackageName())
 							)) continue;
-							
+
 							String packageName = applicationInfo.packageName;
 
 							try{
@@ -229,7 +233,7 @@ public class EMPlugin extends CordovaPlugin {
 								Log.e(TAG, errorString);
 							}
 							applist.put(appinfo);
-							
+
 							break;
 						}
 					}
@@ -238,7 +242,102 @@ public class EMPlugin extends CordovaPlugin {
 				Log.e("Got exception " , e.getMessage());
 			}
 		}
-		
+
 		return applist;
+	}
+
+	public void getLocationIsMock(CallbackContext callbackContext){
+		cordova.getThreadPool().execute(new Runnable() {
+			@Override
+			public void run() {
+				try{
+					locationIsMock(callbackContext);
+				} catch(Exception e){
+					callbackContext.error(e.getMessage());
+				}
+			}
+		});
+	}
+
+	public void locationIsMock(CallbackContext callbackContext) throws Exception{
+		JSONObject result = new JSONObject();
+
+		Location location = null;
+		initLocationManager();
+
+		String source;
+		JSONArray errors = new JSONArray();
+		String errorString = null;
+
+		try{
+			location = locationByProvider(locationManager, LocationManager.GPS_PROVIDER);
+			source = "GPS";
+		} catch(Exception e) {
+			errors.put("GPS error: "+e.getMessage());
+		}
+
+		if(location == null){
+			try{
+				location = locationByProvider(locationManager, LocationManager.NETWORK_PROVIDER);
+				source = "Network";
+			} catch(Exception e) {
+				errors.put("Network error: "+e.getMessage());
+			}
+		}
+
+		if(location != null){
+			boolean isMock = locationIsMock(location);
+
+			try{
+				result.put("isMock", isMock);
+			} catch(JSONException e){
+				errorString = e.getMessage();
+			}
+		} else {
+			errorString = "No location";
+		}
+
+		if(errorString != null){
+			try{
+				result.put("error", errorString);
+				result.put("errors", errors);
+				callbackContext.error(result);
+			} catch(JSONException e){
+				callbackContext.error(e.getMessage());
+			}
+			return;
+		}
+
+		callbackContext.success(result);
+	}
+
+	public boolean locationIsMock(Location location){
+		boolean isMock = false;
+
+		if(Build.VERSION.SDK_INT < 31){
+			isMock = location.isFromMockProvider();
+		}
+
+		return isMock;
+	}
+
+	public Location locationByProvider(LocationManager locationManager, String provider) throws Exception {
+		if(!locationManager.isProviderEnabled(provider)) return null;
+
+		Context context = cordova.getContext();
+		if (
+				ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+						ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+		) {
+			return null;
+		}
+
+		return locationManager.getLastKnownLocation(provider);
+	}
+
+	public void initLocationManager(){
+		if(this.locationManager != null) return;
+
+		locationManager = (LocationManager) this.cordova.getActivity().getSystemService(Context.LOCATION_SERVICE);
 	}
 }
