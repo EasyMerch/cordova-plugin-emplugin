a75a31c229ffd84d653e0294617ec2e523640551
Lordovik <lordovikotaku@gmial.com>
2022-03-28 16:10:09 +0300

    adding scene delegate

---
+++ src/ios/EMSceneDelegate.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : NSObject <UISceneDelegate>{}
+
+@end
---
+++ src/ios/EMSceneDelegate.m
@@ -0,0 +1,15 @@
+#import "SceneDelegate.h"
+
+@implementation SceneDelegate
+
+#pragma mark UISceneDelegate implementation
+
+- (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions{
+	NSLog(@"DEBUG willConnectToSession %@", connectionOptions);
+}
+
+- (void)scene:(UIScene *)scene openURLContexts:(NSSet<UIOpenURLContext *> *)URLContexts{
+	NSLog(@"DEBUG willConnectToSession %@", URLContexts);
+}
+
+@end
