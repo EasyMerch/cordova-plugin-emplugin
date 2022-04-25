faaed5febf93c6416d41eea619646dd661d55ae0
Lordovik <lordovikotaku@gmial.com>
2022-04-25 18:18:51 +0300

    time change reciever

--- src/android/TimeChangeReceiver.java
+++ src/android/TimeChangeReceiver.java
@@ -3,12 +3,12 @@ package ru.pronetcom.easymerch2.emplugin;
 import android.content.BroadcastReceiver;
 import android.util.Log;
 
-public class TimeChangeReciever extends BroadcastReceiver{
+public class TimeChangeReceiver extends BroadcastReceiver{
 	public static int changesCount = 0;
 
 	@Override
     public void onReceive(Context context, Intent intent) {
-		TimeChangeReciever.changesCount++;
-		Log.i("TimeChangeReciever", "changed");
+		TimeChangeReceiver.changesCount++;
+		Log.i("TimeChangeReceiver", "changed");
     }
 }
